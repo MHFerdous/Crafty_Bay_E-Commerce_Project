290ab558ee1ba21188f470d3203f9bdbecf62339
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-09-18 19:27:01 +0600

    Updated

--- lib/presentation/ui/screens/product_details_screen.dart
+++ lib/presentation/ui/screens/product_details_screen.dart
@@ -1,8 +1,30 @@
+import 'package:crafty_bay/presentation/ui/widgets/custom_stepper.dart';
 import 'package:flutter/material.dart';
+import '../utility/app_colors.dart';
 import '../widgets/home/product_image_slider.dart';
 
-class ProductDetailsScreen extends StatelessWidget {
+class ProductDetailsScreen extends StatefulWidget {
   const ProductDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  List<Color> colors = [
+    Colors.black,
+    Colors.grey,
+    Colors.green,
+    Colors.blue,
+  ];
+
+  List<String> sizes = [
+    'mini',
+    'pro',
+  ];
+
+  int _selectedColorIndex = 0;
+  int _selectedSizeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +47,194 @@ class ProductDetailsScreen extends StatelessWidget {
                   elevation: 0,
                 ),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          'Infinix Hot 10S',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                      ),
+                      CustomStepper(
+                          lowerLimit: 1,
+                          upperLimit: 10,
+                          stepValue: 1,
+                          value: 1,
+                          onChange: (newValue) {})
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            size: 22,
+                            color: Colors.amber,
+                          ),
+                          Text(
+                            '4.5',
+                            style: TextStyle(
+                              color: Colors.blueGrey.shade500,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Reviews',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      Card(
+                        color: AppColors.primaryColor,
+                        child: const Padding(
+                          padding: EdgeInsets.all(2),
+                          child: Icon(
+                            Icons.favorite_outline,
+                            size: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const Text(
+                    'Color',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    height: 30,
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: colors.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          borderRadius: BorderRadius.circular(20),
+                          onTap: () {
+                            _selectedColorIndex = index;
+                            if (mounted) {
+                              setState(() {});
+                            }
+                          },
+                          child: CircleAvatar(
+                            radius: 15,
+                            backgroundColor: colors[index],
+                            child: _selectedColorIndex == index
+                                ? CircleAvatar(
+                                    backgroundColor: AppColors.primaryColor,
+                                    child: const Icon(
+                                      Icons.done_outlined,
+                                      color: Colors.white,
+                                    ),
+                                  )
+                                : null,
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const SizedBox(
+                          width: 8,
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const Text(
+                    'Sizes',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    height: 30,
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: sizes.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          borderRadius: BorderRadius.circular(20),
+                          onTap: () {
+                            _selectedSizeIndex = index;
+                            if (mounted) {
+                              setState(() {});
+                            }
+                          },
+                          child: Container(
+                            width: 35,
+                            height: 35,
+                            margin: const EdgeInsets.symmetric(horizontal: 4),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(10),
+                              color: _selectedSizeIndex == index
+                                  ? AppColors.primaryColor
+                                  : null,
+                            ),
+                            alignment: Alignment.center,
+                            child: Text(sizes[index]),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const SizedBox(
+                          width: 8,
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const Text(
+                    'Description',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const Text(
+                    'Description',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
