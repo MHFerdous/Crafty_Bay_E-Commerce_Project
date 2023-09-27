3104d40b519e157a18f86f5a2f177dafcd8016a8
MH Ferdous <mahmudhossainferdous2001@gmail.com>
2023-09-27 20:47:35 +0600

    Updated

--- lib/presentation/ui/screens/product_details_screen.dart
+++ lib/presentation/ui/screens/product_details_screen.dart
@@ -1,7 +1,9 @@
 import 'package:crafty_bay/presentation/ui/widgets/custom_stepper.dart';
 import 'package:flutter/material.dart';
 import '../utility/app_colors.dart';
+import '../widgets/color_picker.dart';
 import '../widgets/home/product_image_slider.dart';
+import '../widgets/size_picker.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
   const ProductDetailsScreen({Key? key}) : super(key: key);
@@ -42,198 +44,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                         productDetailsAppBar,
                       ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              const Expanded(
-                                child: Text(
-                                  'Infinix Hot 10S',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w600,
-                                    letterSpacing: 0.5,
-                                  ),
-                                ),
-                              ),
-                              CustomStepper(
-                                  lowerLimit: 1,
-                                  upperLimit: 10,
-                                  stepValue: 1,
-                                  value: 1,
-                                  onChange: (newValue) {})
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Wrap(
-                                crossAxisAlignment: WrapCrossAlignment.center,
-                                children: [
-                                  const Icon(
-                                    Icons.star,
-                                    size: 22,
-                                    color: Colors.amber,
-                                  ),
-                                  Text(
-                                    '4.5',
-                                    style: TextStyle(
-                                      color: Colors.blueGrey.shade500,
-                                      fontSize: 18,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              TextButton(
-                                onPressed: () {},
-                                child: const Text(
-                                  'Reviews',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                  ),
-                                ),
-                              ),
-                              Card(
-                                color: AppColors.primaryColor,
-                                child: const Padding(
-                                  padding: EdgeInsets.all(2),
-                                  child: Icon(
-                                    Icons.favorite_outline,
-                                    size: 18,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          const Text(
-                            'Color',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          SizedBox(
-                            height: 30,
-                            child: ListView.separated(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: colors.length,
-                              itemBuilder: (context, index) {
-                                return InkWell(
-                                  borderRadius: BorderRadius.circular(20),
-                                  onTap: () {
-                                    _selectedColorIndex = index;
-                                    if (mounted) {
-                                      setState(() {});
-                                    }
-                                  },
-                                  child: CircleAvatar(
-                                    radius: 15,
-                                    backgroundColor: colors[index],
-                                    child: _selectedColorIndex == index
-                                        ? CircleAvatar(
-                                            backgroundColor:
-                                                AppColors.primaryColor,
-                                            child: const Icon(
-                                              Icons.done_outlined,
-                                              color: Colors.white,
-                                            ),
-                                          )
-                                        : null,
-                                  ),
-                                );
-                              },
-                              separatorBuilder:
-                                  (BuildContext context, int index) {
-                                return const SizedBox(
-                                  width: 8,
-                                );
-                              },
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          const Text(
-                            'Sizes',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          SizedBox(
-                            height: 30,
-                            child: ListView.separated(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: sizes.length,
-                              itemBuilder: (context, index) {
-                                return InkWell(
-                                  borderRadius: BorderRadius.circular(20),
-                                  onTap: () {
-                                    _selectedSizeIndex = index;
-                                    if (mounted) {
-                                      setState(() {});
-                                    }
-                                  },
-                                  child: Container(
-                                    width: 35,
-                                    height: 35,
-                                    margin: const EdgeInsets.symmetric(
-                                        horizontal: 4),
-                                    decoration: BoxDecoration(
-                                      border: Border.all(color: Colors.grey),
-                                      borderRadius: BorderRadius.circular(10),
-                                      color: _selectedSizeIndex == index
-                                          ? AppColors.primaryColor
-                                          : null,
-                                    ),
-                                    alignment: Alignment.center,
-                                    child: Text(sizes[index]),
-                                  ),
-                                );
-                              },
-                              separatorBuilder:
-                                  (BuildContext context, int index) {
-                                return const SizedBox(
-                                  width: 8,
-                                );
-                              },
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          const Text(
-                            'Description',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          const Text(
-                            'Infinix Hot 10s price starts from BDT. 12,990. Infinix Hot 10s internal storage base variant of 4 GB, 6 GB Ram, 64 GB, 128 GB Internal Memory (ROM). Infinix Hot 10s which is available in Black, Purple, Morandi Green, Heart of Ocean colour.',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    productDetails,
                   ],
                 ),
               ),
@@ -241,6 +52,144 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
             addToCartBottomContainer
           ],
         ),
+      ),
+    );
+  }
+
+  Padding get productDetails {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const Expanded(
+                child: Text(
+                  'Infinix Hot 10S',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 0.5,
+                  ),
+                ),
+              ),
+              CustomStepper(
+                  lowerLimit: 1,
+                  upperLimit: 10,
+                  stepValue: 1,
+                  value: 1,
+                  onChange: (newValue) {})
+            ],
+          ),
+          Row(
+            children: [
+              Wrap(
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.star,
+                    size: 22,
+                    color: Colors.amber,
+                  ),
+                  Text(
+                    '4.5',
+                    style: TextStyle(
+                      color: Colors.blueGrey.shade500,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Reviews',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Card(
+                color: AppColors.primaryColor,
+                child: const Padding(
+                  padding: EdgeInsets.all(2),
+                  child: Icon(
+                    Icons.favorite_outline,
+                    size: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Text(
+            'Color',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          SizedBox(
+            height: 30,
+            child: ColorPicker(
+              colors: colors,
+              onSelected: (int selectIndex) {
+                _selectedColorIndex = selectIndex;
+              },
+              initialSelected: 0,
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Text(
+            'Sizes',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          SizedBox(
+            height: 30,
+            child: SizePicker(
+              sizes: sizes,
+              onSelected: (int selectIndex) {
+                _selectedSizeIndex = selectIndex;
+              },
+              initialSelected: 0,
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Text(
+            'Description',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Text(
+            'Infinix Hot 10s price starts from BDT. 12,990. Infinix Hot 10s internal storage base variant of 4 GB, 6 GB Ram, 64 GB, 128 GB Internal Memory (ROM). Infinix Hot 10s which is available in Black, Purple, Morandi Green, Heart of Ocean colour.',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey,
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -308,5 +257,3 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
     );
   }
 }
-
-
---
+++ lib/presentation/ui/widgets/size_picker.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import '../utility/app_colors.dart';
+
+class SizePicker extends StatefulWidget {
+  const SizePicker(
+      {Key? key,
+        required this.sizes,
+        required this.onSelected,
+        required this.initialSelected})
+      : super(key: key);
+
+  final List<String> sizes;
+  final Function(int selectIndex) onSelected;
+  final int initialSelected;
+
+  @override
+  State<SizePicker> createState() => _SizePickerState();
+}
+
+class _SizePickerState extends State<SizePicker> {
+  int _selectedSizeIndex = 0;
+
+  @override
+  void initState() {
+    _selectedSizeIndex = widget.initialSelected;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: widget.sizes.length,
+      itemBuilder: (context, index) {
+        return InkWell(
+          borderRadius: BorderRadius.circular(20),
+          onTap: () {
+            _selectedSizeIndex = index;
+            widget.onSelected(index);
+            if (mounted) {
+              setState(() {});
+            }
+          },
+          child: Container(
+            width: 35,
+            height: 35,
+            margin: const EdgeInsets.symmetric(horizontal: 4),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+              color:
+              _selectedSizeIndex == index ? AppColors.primaryColor : null,
+            ),
+            alignment: Alignment.center,
+            child: Text(widget.sizes[index]),
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const SizedBox(
+          width: 8,
+        );
+      },
+    );
+  }
+}
