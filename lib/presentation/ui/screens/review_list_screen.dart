531a006a9b8e6572f093f9916164818a26665607
MHFerdous <mahmudhossainferdous@gmail.com>
2023-10-10 00:01:40 +0600

    Updated

--- lib/presentation/ui/screens/review_list_screen.dart
+++ lib/presentation/ui/screens/review_list_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../utility/app_colors.dart';
 
 class ReviewListScreen extends StatefulWidget {
   const ReviewListScreen({super.key});
@@ -23,6 +26,129 @@ class _ReviewListScreenState extends State<ReviewListScreen> {
         ),
         backgroundColor: Colors.white,
         elevation: 2,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return reviewCard;
+                    },
+                  ),
+                  addToCartBottomContainer(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+
+    );
+  }
+
+  Padding get reviewCard {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        // onTap: .
+        child: Card(
+          color: Colors.grey.shade100,
+          elevation: 3,
+          child:  SizedBox(
+            height: 150,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.person),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Text(
+                        'MH Ferdous',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                    child: Text(
+                      'description The analogies and problems used in these experiments were not specific to any domain of expertise and used fantasy problems relying only on linguistic descriptions. The semantic descriptions of the devices were varied, but the pictures were identical for both conditions. ',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 0.3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      maxLines: 4,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container addToCartBottomContainer() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      decoration: BoxDecoration(
+        color: AppColors.primaryColor.withOpacity(0.20),
+        borderRadius: const BorderRadius.only(
+          topRight: Radius.circular(16),
+          topLeft: Radius.circular(16),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Price',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(
+                //'\$ /*${productDetails.product?.price ?? 0}*/',
+                '\$ 1000',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 120,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('Add to Cart'),
+            ),
+          )
+        ],
       ),
     );
   }
