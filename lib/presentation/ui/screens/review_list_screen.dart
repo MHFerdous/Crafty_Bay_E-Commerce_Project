1e0ce3397c9bad47accb3b832420e9b984dde560
MHFerdous <mahmudhossainferdous@gmail.com>
2023-10-11 22:24:31 +0600

    Updated

--- lib/presentation/ui/screens/review_list_screen.dart
+++ lib/presentation/ui/screens/review_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../utility/app_colors.dart';
 
 class ReviewListScreen extends StatefulWidget {
   const ReviewListScreen({super.key});
@@ -12,9 +13,9 @@ class _ReviewListScreenState extends State<ReviewListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         leading:  const BackButton(
-           color: Colors.black,
-         ),
+        leading: const BackButton(
+          color: Colors.black,
+        ),
         title: const Text(
           'Reviews',
           style: TextStyle(
@@ -34,9 +35,47 @@ class _ReviewListScreenState extends State<ReviewListScreen> {
               },
             ),
           ),
+          addReviewBottomContainer()
         ],
       ),
     );
+  }
+
+  Container addReviewBottomContainer() {
+    return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+          decoration: BoxDecoration(
+            color: AppColors.primaryColor.withOpacity(0.20),
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(16),
+              topLeft: Radius.circular(16),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Reviews (10)',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              ),
+              CircleAvatar(
+                radius: 22,
+                backgroundColor: AppColors.primaryColor,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
   }
 
   Padding get reviewCard {
@@ -45,44 +84,39 @@ class _ReviewListScreenState extends State<ReviewListScreen> {
       child: Card(
         color: Colors.grey.shade100,
         elevation: 3,
-        child: const SizedBox(
-          height: 150,
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Icon(Icons.person),
-                    SizedBox(
-                      width: 16,
-                    ),
-                    Text(
-                      'MH Ferdous',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Expanded(
-                  child: Text(
-                    'description The analogies and problems used in these experiments were not specific to any domain of expertise and used fantasy problems relying only on linguistic descriptions. The semantic descriptions of the devices were varied, but the pictures were identical for both conditions. ',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      letterSpacing: 0.3,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    maxLines: 4,
+        child: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.person),
+                  SizedBox(
+                    width: 16,
                   ),
+                  Text(
+                    'MH Ferdous',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'description The analogies and problems used in these experiments were not specific to any domain of expertise and used fantasy problems relying only on linguistic descriptions. The semantic descriptions of the devices were varied, but the pictures were identical for both conditions. ',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0.3,
+                  // overflow: TextOverflow.ellipsis,
                 ),
-              ],
-            ),
+                // maxLines: 4,
+              ),
+            ],
           ),
         ),
       ),
