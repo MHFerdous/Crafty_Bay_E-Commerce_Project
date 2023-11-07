a5835a90c0811c98fe6c70584afc82d873f0ee63
MHFerdous <mahmudhossainferdous@gmail.com>
2023-11-07 18:00:03 +0600

    bug fixed

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -94,7 +94,7 @@ class _CraftyBayState extends State<CraftyBay> {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch:
-            MaterialColor(AppColors.primaryColor.value, AppColors().color),
+        MaterialColor(AppColors.primaryColor.value, AppColors().color),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             padding: const EdgeInsets.symmetric(vertical: 12),
